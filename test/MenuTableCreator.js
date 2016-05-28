133d0a2b3c4850c3980dc598baf7ffa319087cf9
jaebradley <jae.b.bradley@gmail.com>
2016-05-28 16:58:17 -0400

    header test

--- test/MenuTableCreator.js
+++ test/MenuTableCreator.js
@@ -8,11 +8,14 @@ import MenuType from '../src/data/MenuType';
 
 describe('Menu table creator', function() {
   const tableCreator = new MenuTableCreator();
+  const expectedPriceEmoji = emoji.get('money_with_wings');
+  const expectedMemoEmoji = emoji.get('memo');
+  const expectedColWidths = [20, 10, 50];
 
   it('tests constructor', function() {
-    expect(tableCreator.priceEmoji).to.equal(emoji.get('money_with_wings'));
-    expect(tableCreator.memoEmoji).to.equal(emoji.get('memo'));
-    expect(tableCreator.colWidths).to.eql([20, 10, 50]);
+    expect(tableCreator.priceEmoji).to.equal(expectedPriceEmoji);
+    expect(tableCreator.memoEmoji).to.equal(expectedMemoEmoji);
+    expect(tableCreator.colWidths).to.eql(expectedColWidths);
     expect(tableCreator.menuTypeOrder).to.eql([
       MenuType.ENTREES,
       MenuType.COMBINATIONS,
@@ -24,6 +27,11 @@ describe('Menu table creator', function() {
   });
 
   it('tests header generation', function() {
+    const expectedHeader = ['jae', expectedPriceEmoji, expectedMemoEmoji];
+    expect(tableCreator.generateHeader('jae')).to.eql(expectedHeader);
+  });
+
+  it('tests table creation', function() {
 
   });
 });
