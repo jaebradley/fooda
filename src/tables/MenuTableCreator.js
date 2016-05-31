e0d45b859d184fc0e1309e4e220f79a402ef3ba7
jaebradley <jae.b.bradley@gmail.com>
2016-05-31 00:51:31 -0400

    updated highlighting

--- src/tables/MenuTableCreator.js
+++ src/tables/MenuTableCreator.js
@@ -2,8 +2,10 @@
 
 import Table from 'cli-table2';
 import emoji from 'node-emoji';
+import colors from 'colors';
 
 import MenuType from '../data/MenuType';
+import DietaryRestrictions from '../data/DietaryRestrictions';
 
 export default class MenuTableCreator {
   constructor() {
@@ -18,18 +20,32 @@ export default class MenuTableCreator {
       MenuType.SIDES,
       MenuType.SIDES_AND_DESSERT,
     ];
+    this.footer = ['Vegetarian'.green, 'Gluten Free'.yellow, 'Vegetarian & Gluten Free'.magenta];
   }
 
   generateHeader(vendor) {
     return [vendor, this.priceEmoji, this.memoEmoji];
   }
 
+  generateFormattedRow(name, price, description, labels) {
+    if (labels.indexOf(DietaryRestrictions.VEGETARIAN) > -1 && labels.indexOf(DietaryRestrictions.GLUTEN_FREE) > -1) {
+      return [name.magenta, price.magenta, description.magenta];
+    } else if (labels.indexOf(DietaryRestrictions.VEGETARIAN) > -1) {
+      return [name.green, price.green, description.green];
+    } else if (labels.indexOf(DietaryRestrictions.GLUTEN_FREE) > -1) {
+      return [name.yellow, price.yellow, description.yellow];
+    } else {
+      return [name, price, description];
+    }
+  }
+
   create(menu) {
     const table = new Table({head: this.generateHeader(menu.vendor), colWidths: this.colWidths, wordWrap: true});
     this.menuTypeOrder.map(function(menuType) {
       let items = menu.get(menuType);
-      items.map(item => table.push([item.name, item.price, item.description]));
+      items.map(item => table.push(this.generateFormattedRow(item.name, item.price, item.description, item.labels)));
     });
+    table.push(this.footer);
     return table.toString();
   }
 };
