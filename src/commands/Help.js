415283681dd9464114b859b2777412a1f1889a69
jaebradley <jae.b.bradley@gmail.com>
2016-06-02 21:27:36 -0400

    formatting change

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -3,7 +3,7 @@
 import LocationOption from '../data/LocationOption';
 
 const helpText = `To get Fooda data, type fooda menu [location_option]. For example, fooda menu davenport.\n`;
-const locationOptionText = `Location options are: ${LocationOption.HUBSPOT.toLowerCase()}, ${LocationOption.DAVENPORT.toLowerCase()}\n`;
+const locationOptionText = `Location options are: ${LocationOption.HUBSPOT.toLowerCase()}, ${LocationOption.DAVENPORT.toLowerCase()}`;
 
 export default {
   TEXT: `${helpText}${locationOptionText}`,
