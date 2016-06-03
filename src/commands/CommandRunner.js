f90786be8840805c1e85a8e2be1919ea76791053
jaebradley <jae.b.bradley@gmail.com>
2016-06-02 21:27:08 -0400

    bug fix

--- src/commands/CommandRunner.js
+++ src/commands/CommandRunner.js
@@ -1,20 +1,42 @@
 'use es6';
 
 import program from 'commander';
+import emoji from 'node-emoji';
 import MenuCommand from './MenuCommand';
+import Help from './Help';
+import LocationOption from '../data/LocationOption';
 
 export default class CommandRunner {
   constructor() {
-    this.menuCommand = new MenuCommand()
+    this.menuCommand = new MenuCommand();
+    this.screamEmoji = emoji.get('scream');
+    this.astonishedEmoji = emoji.get('astonished');
+    this.rutroEmojis = `${this.screamEmoji} ${this.astonishedEmoji} ${this.screamEmoji} ${this.astonishedEmoji}`;
   }
 
   run() {
     program.version("0.0.1");
 
+    program.on('help', function() { 
+        console.log(Help.TEXT);
+        process.exit(1); 
+    });
+
     program.command("menu [location]")
             .description("get fooda data")
-            .action(location => this.menuCommand.run(location))
+            .action((function(location) {
+              if (typeof location == 'undefined' || !LocationOption.isValid(location)) {
+                console.log(`${this.rutroEmojis} ${location} is not a valid location ${this.rutroEmojis}`);
+                console.log(Help.TEXT);
+              } else {
+                this.menuCommand.run(location);
+              }
+            }).bind(this));
 
     program.parse(process.argv);
+
+    if (program.args.length === 0) {
+      console.log(Help.TEXT);
+    }
   }
 }
--- src/data/LocationOption.js
+++ src/data/LocationOption.js
@@ -1,6 +1,13 @@
 'use es6';
 
+const HUBSPOT = 'HUBSPOT';
+const DAVENPORT = 'DAVENPORT';
+const OPTIONS = [HUBSPOT, DAVENPORT];
+
 export default {
-  HUBSPOT: 'HUBSPOT',
-  DAVENPORT: 'DAVENPORT',
+  HUBSPOT: HUBSPOT,
+  DAVENPORT: DAVENPORT,
+  isValid: function(candidate) {
+    return OPTIONS.indexOf(candidate.toUpperCase()) > -1;
+  },
 };
