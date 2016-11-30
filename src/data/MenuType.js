68ee3020e356dea15380bbeb0238e900ad462fd1
jaebradley <jae.b.bradley@gmail.com>
2016-11-30 01:22:48 -0500

    fucking tests are fixed

--- src/data/MenuType.js
+++ src/data/MenuType.js
@@ -9,30 +9,37 @@ export default class MenuType extends Enum {};
 MenuType.initEnum({
   COMBINATIONS: {
     value: 'combinations',
+    display: 'Combinations',
     emoji: `${emoji.get('hamburger')} ${emoji.get('fries')} ${emoji.get('beer')}`,
   },
   DESSERTS: {
     value: 'desserts',
+    display: 'Desserts',
     emoji: emoji.get('cake'),
   },
   ENTREES: {
     value: 'entrees',
+    display: 'Entrees',
     emoji: emoji.get('pizza'),
   },
   SANDWICHES: {
     value: 'sandwiches',
+    display: 'Sandwiches',
     emoji: emoji.get('hamburger'),
   },
   SALADS: {
     value: 'salads',
+    display: 'Salads',
     emoji: `${emoji.get('herb')} ${emoji.get('tomato')}`
   },
   SIDES: {
     value: 'sides',
+    display: 'Sides',
     emoji: emoji.get('fries'),
   },
   SIDES_AND_DESSERT: {
     value: 'sidesAndDessert',
+    display: 'Sides And Dessert',
     emoji: `${emoji.get('fries')} ${emoji.get('cake')}`,
   },
 });
--- src/tables/MenuTableCreator.js
+++ src/tables/MenuTableCreator.js
@@ -9,28 +9,6 @@ import MenuType from '../data/MenuType';
 import DietaryRestrictions from '../data/DietaryRestrictions';
 
 export default class MenuTableCreator {
-  constructor() {
-    this.priceEmoji = emoji.get('moneybag');
-    this.memoEmoji = emoji.get('memo');
-    this.dessertEmoji = emoji.get('cake');
-    this.friesEmoji = emoji.get('fries');
-    this.hamburgerEmoji = emoji.get('hamburger');
-    this.herbEmoji = emoji.get('herb');
-    this.tomatoEmoji = emoji.get('tomato');
-    this.beerEmoji = emoji.get('beer');
-    this.pizzaEmoji = emoji.get('pizza');
-    this.colWidths = [null, 10, 50];
-    this.menuTypeOrder = [
-      MenuType.ENTREES,
-      MenuType.COMBINATIONS,
-      MenuType.SANDWICHES,
-      MenuType.SALADS,
-      MenuType.SIDES,
-      MenuType.SIDES_AND_DESSERT,
-      MenuType.DESSERTS,
-    ];
-    this.footer = [{colSpan: 3, content: `${'Vegetarian'.green}\n${'Gluten Free'.yellow}\n${'Vegetarian & Gluten Free'.magenta}`}];
-  }
 
   static getColumnWidths() {
     return [null, 10, 50];
@@ -60,15 +38,15 @@ export default class MenuTableCreator {
     );
   }
 
-  generateHeader(vendor, date) {
+  static generateHeader(vendor, date) {
     return [`${vendor} (${date})`, MenuTableCreator.getPriceEmoji(), MenuTableCreator.getMemoEmoji()];
   }
 
-  generateMenuTypeHeader(menuType) {
-    return [{content: `${menuType.value.toUpperCase().cyan} ${menuType.emoji}`, colSpan: 3}];
+  static generateMenuTypeHeader(menuType) {
+    return [{content: `${menuType.display.toUpperCase().cyan} ${menuType.emoji}`, colSpan: 3}];
   }
 
-  generateFormattedRow(name, price, description, labels) {
+  static generateFormattedRow(name, price, description, labels) {
     if (labels.indexOf(DietaryRestrictions.VEGETARIAN) > -1 && labels.indexOf(DietaryRestrictions.GLUTEN_FREE) > -1) {
       return [name.magenta, price.magenta, description.magenta];
     } else if (labels.indexOf(DietaryRestrictions.VEGETARIAN) > -1) {
@@ -80,16 +58,20 @@ export default class MenuTableCreator {
     };
   }
 
-  create(menu) {
-    let table = new Table({head: this.generateHeader(menu.vendor, menu.date), colWidths: this.colWidths, wordWrap: true,});
-    this.menuTypeOrder.map((function(menuType) {
-      let items = menu.get(menuType);
-      if (items.length > 0) {
-        table.push(this.generateMenuTypeHeader(menuType));
+  static create(menu) {
+    let table = new Table({
+        head: MenuTableCreator.generateHeader(menu.vendor, menu.date),
+        colWidths: MenuTableCreator.getColumnWidths(),
+        wordWrap: true,
+    });
+    MenuTableCreator.getMenuOrder().forEach((function(menuType) {
+      let items = menu.get(menuType.name);
+      if (items.size > 0) {
+        table.push(MenuTableCreator.generateMenuTypeHeader(menuType));
       }
-      items.map(item => table.push(this.generateFormattedRow(item.name, item.price, item.description, item.labels)));
+      items.forEach(item => table.push(MenuTableCreator.generateFormattedRow(item.name, item.price, item.description, item.labels)));
     }).bind(this));
-    table.push(this.footer);
+    table.push(MenuTableCreator.getFooter());
     return table.toString();
   }
 };
